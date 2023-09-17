186802ea9112128f1b68bb9cc1eb6c819ee2fd6c
ki8vi <oneofbln@gmail.com>
2023-09-17 20:43:17 +0600

    feat: task complete

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,27 @@
 class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
-    }
-
-    guess() {
-
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+    constructor() {
+        this.maxNumber;
+        this.minNumber;
+        this.number;
+      }
+      
+      setRange(min, max) {
+        this.maxNumber = max;
+        this.minNumber = min;
+      }
+    
+      guess() {
+        this.number = Math.round((this.minNumber + this.maxNumber) / 2)
+        return this.number;
+      }
+    
+      lower() {
+        this.maxNumber = this.number;
+      }
+    
+      greater() {
+        this.minNumber = this.number;
+      }  
 }
 
 module.exports = GuessingGame;
